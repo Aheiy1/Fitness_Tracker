890eb01f0abcc2e4edaea1dea5c546f802f985da
Aheiy1 <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>
2022-04-05 13:21:28 -0500

    client.connect and created tables

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // create the express server here
+// const {PORT = 3000} = process.env
+// const express = require('express')
+// const server = express();
+// const morgan = require('morgan')
+// const client = require('./db/client');
+
+
+// require('dotenv').config();
+// client.connect();
+// server.use(morgan("dev"));
+// server.use(express.json());
+
+
+// server.listen(PORT, () => {
+//     console.log("the server is running", PORT);
+//   });
