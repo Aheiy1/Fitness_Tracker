b98d90091d97c1c3bdea01f76085ba6edb46bbbb
Aheiy1 <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>
2022-04-08 13:06:32 -0500

    register working with token

--- api/users.js
+++ api/users.js
@@ -5,32 +5,68 @@ const { JWT_SECRET } = process.env;
 const { getUserByUsername, createUser, getUser } = require("../db");
 
 usersRouter.post("/register", async (req, res, next) => {
-  const { username, password } = req.body;
-
   try {
-    if (password.length < 8) {
-      return res.status(400).json({
-        name: "password error",
-        message: "Password is too short",
-      });
-    }
+    const { username, password } = req.body;
     const _user = await getUserByUsername(username);
     if (_user) {
-      return res.status(400).json({
+      res.status(401);
+      next({
         name: "UserExistsError",
         message: "A user by that username already exists",
       });
+    } else if (password.length < 8) {
+      res.status(401);
+      next({ name: "PasswordLenghtError", message: "Passwrod Too Short!" });
     } else {
-      const user = await createUser({
-        username,
-        password,
-      });
-      res.send({ user });
+      const user = await createUser({ username, password });
+      if (!user) {
+        next({
+          name: "UserCreationError",
+          message: "There was a probelm registering you. Please try again.",
+        });
+      } else {
+        // json token
+        const token = jwt.sign(
+          { id: user.id, username: username },
+          process.env.JWT_SECRET
+        );
+        res.send({ user ,token, message: "you're logged in!" });
+      }
     }
   } catch (error) {
     next(error);
   }
 });
+
+// usersRouter.post("/register", async (req, res, next) => {
+//   const { username, password } = req.body;
+
+//   try {
+//     if (password.length < 8) {
+//       res.status(401);
+//      next ({
+//         name: "password error",
+//         message: "Password is too short",
+//       });
+//     }
+//     const _user = await getUserByUsername(username);
+//     if (_user) {
+//       res.status(401);
+//      next({
+//         name: "UserExistsError",
+//         message: "A user by that username already exists",
+//       });
+//     } else {
+//       const user = await createUser({
+//         username,
+//         password,
+//       });
+//       res.send({ user });
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 usersRouter.post("/login", async (req, res, next) => {
   // console.log(req, "request");
